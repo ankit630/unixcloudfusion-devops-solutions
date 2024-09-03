79771dd907abb628d5eef6102b4f34b90f4bfd8c
Ankit Mittal <ankit.mittal@nsearchglobal.com>
2024-09-03 22:34:38 +0800

    Update ECR module with correct resource declarations

---
+++ terraform-modules/ecr/main.tf
@@ -0,0 +1,27 @@
+resource "aws_ecr_repository" "repo" {
+  name                 = var.repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "repo-policy" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 10 images"
+      action = {
+        type = "expire"
+      }
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 10
+      }
+    }]
+  })
+}
