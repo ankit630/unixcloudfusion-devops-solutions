981aba3b98a6ad845e64b2b36d0b9a429219798f
Ankit Mittal <ankit.mittal@nsearchglobal.com>
2024-09-06 21:46:07 +0800

    updated the efs module

---
+++ terraform-modules/efs/variables.tf
@@ -0,0 +1,32 @@
+variable "creation_token" {
+  description = "A unique name for the EFS file system"
+  type        = string
+}
+
+variable "encrypted" {
+  description = "Whether to enable encryption for the EFS file system"
+  type        = bool
+  default     = true
+}
+
+variable "transition_to_ia" {
+  description = "Lifecycle policy for transition to IA storage class"
+  type        = string
+  default     = "AFTER_30_DAYS"
+}
+
+variable "tags" {
+  description = "Tags to apply to the EFS file system"
+  type        = map(string)
+  default     = {}
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs to create mount targets in"
+  type        = list(string)
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs to apply to the mount targets"
+  type        = list(string)
+}
