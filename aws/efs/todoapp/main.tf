0acab0d00578d01d371f19e2b0d9eca312ff29fd
Ankit Mittal <ankit.mittal@nsearchglobal.com>
2024-09-08 09:02:14 +0800

    updated efs

--- aws/efs/todoapp/main.tf
+++ aws/efs/todoapp/main.tf
@@ -2,39 +2,29 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_vpc" "eks" {
-  filter {
-    name   = "tag:Name"
-    values = ["eksctl-dev-cluster-cluster/VPC"]
-  }
+data "aws_eks_cluster" "dev_cluster" {
+  name = "dev-cluster"
 }
 
-data "aws_subnets" "eks" {
+data "aws_vpc" "eks_vpc" {
+  id = data.aws_eks_cluster.dev_cluster.vpc_config[0].vpc_id
+}
+
+data "aws_subnets" "private" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.eks.id]
+    values = [data.aws_vpc.eks_vpc.id]
   }
-}
 
-data "aws_subnet" "eks" {
-  for_each = toset(data.aws_subnets.eks.ids)
-  id       = each.value
-}
-
-locals {
-  # Get one subnet per AZ
-  unique_az_subnets = { for az, subnets in {
-    for subnet in data.aws_subnet.eks : subnet.availability_zone => subnet.id...
-  } : az => subnets[0] }
+  tags = {
+    Type = "private"
+  }
 }
 
 module "efs" {
-  source = "git::https://github.com/ankit630/unixcloudfusion-devops-solutions.git//terraform-modules/efs?ref=efs-v1.0.4"
+  source = "git::https://github.com/ankit630/unixcloudfusion-devops-solutions.git//terraform-modules/efs?ref=efs-v1.0.5"
 
-  creation_token     = var.efs_name
-  encrypted          = var.efs_encrypted
-  transition_to_ia   = var.efs_transition_to_ia
-  tags               = var.efs_tags
-  subnet_ids         = data.aws_subnets.eks.ids
+  subnet_ids         = data.aws_subnets.private.ids
   vpc_id             = data.aws_vpc.eks.id
+  efs_name           = "todoapp-efs"
 }
--- aws/efs/todoapp/outputs.tf
+++ aws/efs/todoapp/outputs.tf
@@ -1,3 +1,8 @@
+
 output "efs_id" {
   value = module.efs.efs_id
+}
+
+output "efs_dns_name" {
+  value = module.efs.efs_dns_name
 }
