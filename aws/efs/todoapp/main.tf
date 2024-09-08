502884336d0cd98acaa26b4b2788a111141dbee6
Ankit Mittal <ankit.mittal@nsearchglobal.com>
2024-09-08 08:46:08 +0800

    updated efs module

--- aws/efs/todoapp/main.tf
+++ aws/efs/todoapp/main.tf
@@ -16,8 +16,20 @@ data "aws_subnets" "eks" {
   }
 }
 
+data "aws_subnet" "eks" {
+  for_each = toset(data.aws_subnets.eks.ids)
+  id       = each.value
+}
+
+locals {
+  # Get one subnet per AZ
+  unique_az_subnets = { for az, subnets in {
+    for subnet in data.aws_subnet.eks : subnet.availability_zone => subnet.id...
+  } : az => subnets[0] }
+}
+
 module "efs" {
-  source = "git::https://github.com/ankit630/unixcloudfusion-devops-solutions.git//terraform-modules/efs?ref=efs-v1.0.3"
+  source = "git::https://github.com/ankit630/unixcloudfusion-devops-solutions.git//terraform-modules/efs?ref=efs-v1.0.4"
 
   creation_token     = var.efs_name
   encrypted          = var.efs_encrypted
