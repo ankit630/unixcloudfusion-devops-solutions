c134ae6c684ec316aed86d60b27f17df710e0349
Ankit Mittal <ankit.mittal@nsearchglobal.com>
2024-09-08 09:50:23 +0800

    updated version

--- aws/efs/todoapp/main.tf
+++ aws/efs/todoapp/main.tf
@@ -31,16 +31,11 @@ data "aws_subnets" "private" {
   }
 }
 
-data "aws_vpc" "selected" {
-  id = data.aws_vpc.eks_vpc.id
-}
-
 module "efs" {
-  source       = "git::https://github.com/ankit630/unixcloudfusion-devops-solutions.git//terraform-modules/efs?ref=efs-v1.0.6"
+  source       = "git::https://github.com/ankit630/unixcloudfusion-devops-solutions.git//terraform-modules/efs?ref=efs-v1.0.8"
   vpc_id       = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
   subnet_ids   = data.aws_subnets.private.ids
   efs_name     = var.efs_name
-  cidr_block   = data.aws_vpc.selected.cidr_block
   tags       = {
     Environment = "dev"
     Project     = "gitlab-runner"
