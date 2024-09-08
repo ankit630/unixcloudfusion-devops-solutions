85439f0a7b9faf2b7db620374590ceccca20063b
Ankit Mittal <ankit.mittal@nsearchglobal.com>
2024-09-08 10:52:56 +0800

    debugging subnets

--- aws/efs/todoapp/outputs.tf
+++ aws/efs/todoapp/outputs.tf
@@ -14,19 +14,20 @@ output "efs_mount_targets" {
   value = module.efs.efs_mount_targets
 }
 
-output "private_subnet_ids" {
-  value = data.aws_subnets.private.ids
-}
-
 output "vpc_id" {
   value = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
 }
 
+output "all_subnet_ids" {
+  value = data.aws_subnets.private.ids
+}
+
 output "subnet_details" {
-  value = [for s in data.aws_subnets.private.ids : {
+  value = {for s in data.aws_subnets.private.ids : s => {
     id   = s
     tags = data.aws_subnet.details[s].tags
-  }]
+    availability_zone = data.aws_subnet.details[s].availability_zone
+  }}
 }
 
 output "all_subnet_ids" {
