e9b7446118e09723e86f3f4aa10e027fc63abb88
Ankit Mittal <ankit.mittal@nsearchglobal.com>
2024-09-08 11:25:46 +0800

    debugging subnets

--- aws/efs/todoapp/main.tf
+++ aws/efs/todoapp/main.tf
@@ -19,17 +19,22 @@ data "aws_vpc" "eks_vpc" {
   id = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
 }
 
-data "aws_subnets" "all" {
+data "aws_subnets" "private" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.eks_vpc.id]
+  }
+
+  filter {
+    name   = "tag:kubernetes.io/role/internal-elb"
+    values = ["1"]
   }
 }
 
 module "efs" {
   source       = "git::https://github.com/ankit630/unixcloudfusion-devops-solutions.git//terraform-modules/efs?ref=efs-v1.0.9"
   vpc_id       = data.aws_eks_cluster.cluster.vpc_config[0].vpc_id
-  subnet_ids   = data.aws_subnets.all.ids
+  subnet_ids   = data.aws_subnets.private.ids
   efs_name     = var.efs_name
   tags       = {
     Environment = "dev"
--- aws/efs/todoapp/outputs.tf
+++ aws/efs/todoapp/outputs.tf
@@ -18,6 +18,6 @@ output "vpc_id" {
   value = data.aws_vpc.eks_vpc.id
 }
 
-output "subnet_ids" {
-  value = data.aws_subnets.all.ids
+output "private_subnet_ids" {
+  value = data.aws_subnets.private.ids
 }
